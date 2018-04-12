54e1c2ab86b55448076c03a6ad51b337b2ea7e39
jihed1 <37672686+jihed1@users.noreply.github.com>
2018-04-12 12:10:49 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
+import { NgModel } from '@angular/forms/src/directives/ng_model';
+import { UppercaseComponent } from './uppercase/uppercase.component';
+import { AppRoutingModule } from './/app-routing.module';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact',  component: ContactComponent },
+  { path: 'uppercase',  component: UppercaseComponent }
+   ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    ContactComponent,
+    UppercaseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule ,
+  RouterModule.forRoot(appRoutes),
+  AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
